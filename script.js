8a3918a0553a4e06fa978ccb3d10c5b4956e9ca0
Chris <Britten63@hotmail.com>
2025-10-03 18:26:45 -0300

    cannot get image to show up on its own using js -- commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalid= null;
+
+document.addEventListener("DOMContentLoaded",initSlider); 
+
+
+
+function initSlider(){
+
+    slides[slideIndex].classList.add("displaySlide");
+}
+function showSlide(index){
+
+}
+function prevSlide(){
+
+
+}
+function nextSlide(){
+
+}
