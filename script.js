26a34ca27685ea6649e7b0a01c1562c26f7f4812
Chris <Britten63@hotmail.com>
2025-10-04 02:00:16 -0300

    img is not both appearing this is an issue. buttons added. function added

--- script.js
+++ script.js
@@ -11,16 +11,36 @@ document.addEventListener("DOMContentLoaded",initSlider);
 
 
 function initSlider(){
+   if(slides.length > 0){
+  slides[slideIndex].classList.add("displaySlide");
+    setInterval(nextSlide, 5000);
+   }
+  
 
-    slides[slideIndex].classList.add("displaySlide");
 }
 function showSlide(index){
 
+     if(index >= slides.length){
+        slideIndex= 0;
+     }
+     else if(index < 0){
+        slideIndex = slides.length - 1
+     }
+
+
+ slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+
+ })
+ slides[slideIndex].classList.add("displaySlide");
 }
 function prevSlide(){
-
+clearInterval(intervalid);
+slideIndex--;
+showSlide(slideIndex);
 
 }
 function nextSlide(){
-
+    slideIndex++;
+    showSlide(slideIndex);
 }
