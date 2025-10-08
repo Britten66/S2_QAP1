80e8e09b238052e432e850c64fe580f0c21512f3
Chris <Britten63@hotmail.com>
2025-10-08 08:57:30 -0300

    table added - requirments being met

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const slides2 = document.querySelectorAll(".slides2 img");
 let slideIndex2 = 0;
 let intervalid2 = null;
 
-// const slides3 = document.querySelectorAll(".slides img");
-// let slideIndex3 = 0;
-// let intervalid3= null;
+const slides3 = document.querySelectorAll(".slides3 img");
+let slideIndex3 = 0;
+let intervalid3 = null;
 
-// const slides4 = document.querySelectorAll(".slides img");
-// let slideIndex4 = 0;
-// let intervalid4= null;
+const slides4 = document.querySelectorAll(".slides4 img");
+let slideIndex4 = 0;
+let intervalid4 = null;
 
 document.addEventListener("DOMContentLoaded", initSlider);
 
@@ -22,11 +22,23 @@ function initSlider() {
 
     intervalid = setInterval(nextSlide, 5000);
   }
-
+  //===== Slide 2 =====
   if (slides2.length > 0) {
     slides2[slideIndex2].classList.add("displaySlide");
 
     intervalid2 = setInterval(nextSlide2, 5000);
+  }
+  //===== Slide 3 =====
+  if (slides3.length > 0) {
+    slides3[slideIndex3].classList.add("displaySlide");
+
+    intervalid3 = setInterval(nextSlide3, 5000);
+  }
+  //===== Slide 4 =====
+  if (slides4.length > 0) {
+    slides4[slideIndex4].classList.add("displaySlide");
+
+    intervalid4 = setInterval(nextSlide4, 5000);
   }
 }
 
@@ -54,6 +66,31 @@ function showSlide2(index) {
   });
   slides2[slideIndex2].classList.add("displaySlide");
 }
+
+//===== Slide 3 =====
+function showSlide3(index) {
+  if (index >= slides3.length) {
+    slideIndex3 = 0;
+  } else if (index < 0) {
+    slideIndex3 = slides3.length - 1;
+  }
+  slides3.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides3[slideIndex3].classList.add("displaySlide");
+}
+//===== Slide 4 =====
+function showSlide4(index) {
+  if (index >= slides4.length) {
+    slideIndex4 = 0;
+  } else if (index < 0) {
+    slideIndex4 = slides4.length - 1;
+  }
+  slides4.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides4[slideIndex4].classList.add("displaySlide");
+}
 // slide 1
 
 function prevSlide() {
@@ -76,4 +113,26 @@ function prevSlide2() {
 function nextSlide2() {
   slideIndex2++;
   showSlide2(slideIndex2);
+}
+//===== Slide 3 =====
+
+function prevSlide3() {
+  clearInterval(intervalid3);
+  slideIndex3--;
+  showSlide3(slideIndex3);
+}
+function nextSlide3() {
+  slideIndex3++;
+  showSlide3(slideIndex3);
+}
+//===== Slide 4 =====
+
+function prevSlide4() {
+  clearInterval(intervalid4);
+  slideIndex4--;
+  showSlide4(slideIndex4);
+}
+function nextSlide4() {
+  slideIndex4++;
+  showSlide4(slideIndex4);
 }
